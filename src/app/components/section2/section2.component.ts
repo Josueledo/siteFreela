dce35a2587c5fd56abf29ad98ebe1e6f61fce608
Josueledo <josueledo12@gmail.com>
2025-04-04 21:41:56 -0300

    Lavei louça na casa da Luiza

--- src/app/components/section2/section2.component.ts
+++ src/app/components/section2/section2.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin()
+
 @Component({
   selector: 'app-section2',
   standalone: true,
   imports: [FontAwesomeModule],
   templateUrl: './section2.component.html',
-  styleUrl: './section2.component.scss'
+  styleUrl: './section2.component.scss',
 })
 export class Section2Component {
-  faArrowUp= faArrowUp
+  faArrowUp = faArrowUp;
+  @ViewChild('box', { static: false }) box!: ElementRef;
+
+  constructor() {
+    gsap.registerPlugin()
+
+  }
+
+
+
+
 }
--- src/app/components/section3/section3.component.ts
+++ src/app/components/section3/section3.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin()
 
 @Component({
   selector: 'app-section3',
@@ -8,5 +14,35 @@ import { Component } from '@angular/core';
   styleUrl: './section3.component.scss'
 })
 export class Section3Component {
+ @ViewChild('box', { static: false }) box!: ElementRef;
+ @ViewChild('box2', { static: false }) box2!: ElementRef;
+ @ViewChild('box3', { static: false }) box3!: ElementRef;
+
+  constructor() {
+    gsap.registerPlugin()
+
+  }
+
+  ngAfterViewInit() {
+    gsap.registerPlugin()
+    this.animation(this.box.nativeElement)
+    this.animation(this.box2.nativeElement)
+    this.animation(this.box3.nativeElement)
+
+  }
+
+  animation(obj:any){
+    gsap.to(obj,{
+      duration:3,
+      scrollTrigger:{
+        trigger:obj,
+        start:"top 80%",
+        end:"top 0px",
+        scrub:true,
+        markers: true,
+        toggleClass:"active"
+      }
+    })
+  }
 
 }

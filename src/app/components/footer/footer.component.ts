00ede4cc214e9b6eed91563cfc7480339b108cdb
Josueledo <josueledo12@gmail.com>
2025-04-20 21:36:20 -0300

    Feat metas tags and FavIcon

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,7 +12,7 @@ export class FooterComponent {
   activeIndex: number | null = null;
 
   faqs = [
-    { question: "O que é?", answer: "Somos uma empresa jovem, dinâmica e focada em consolidar nossa presença no mercado de desenvolvimento web, entregando resultados excepcionais em tudo o que fazemos.", open: false },
+    { question: "Quem somos?", answer: "Somos uma empresa jovem, dinâmica e focada em consolidar nossa presença no mercado de desenvolvimento web, entregando resultados excepcionais em tudo o que fazemos.", open: false },
     { question: "Quanto tempo leva para criar um site?", answer: "O tempo para criar um site varia conforme o tamanho e a complexidade do projeto.", open: false },
     { question: "Posso atualizar meu site depois?", answer: "Com certeza! Mesmo após a entrega, estamos disponíveis para negociar e realizar atualizações conforme necessário.", open: false },
     { question: "Quais tecnologias vocês usam?", answer: "Analisamos suas necessidades para identificar as melhores tecnologias a serem aplicadas em cada projeto.", open: false }
